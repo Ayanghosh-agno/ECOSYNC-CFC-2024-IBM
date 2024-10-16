3352d0d26807cb83127a6711cf3bb6d6c6e4ed6f
Ayan Ghosh <70548601+Ayanghosh-agno@users.noreply.github.com>
2024-10-16 22:10:39 +0530

    Update ecoSyncLoginPageB.js

--- Ecosync Website/main/default/lwc/ecoSyncLoginPageB/ecoSyncLoginPageB.js
+++ Ecosync Website/main/default/lwc/ecoSyncLoginPageB/ecoSyncLoginPageB.js
@@ -3,11 +3,18 @@ export default class EcoSyncLoginPageB extends LightningElement {
     renderedCallback() {
         const ss = this.template.querySelector('video');
         if (ss.paused) {
-            console.log(ss)
-            document.addEventListener('click', () => {
-                ss.play()
-            })
-
+            document.addEventListener('click', this.clickHandler);
+        }else{
+            document.removeEventListener('click', this.clickHandler);
+        }
+    }
+    
+    clickHandler = (event) => {
+        const ssVideo = this.template.querySelector('video');
+        if (ss.paused) {
+            ss.play()
+        }else{
+            document.removeEventListener('click', this.clickHandler);
         }
     }
 }
